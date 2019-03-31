d415ebf360b7389d6463c0e6b399ed251e3ad026
Fabio Ayres <fabioja@insper.edu.br>
2019-03-31 11:31:08 -0300

    Commit inicial.

---
+++ ep1.py
@@ -0,0 +1,86 @@
+# EP 2019-1: Escape Insper
+#
+# Alunos: 
+# - aluno A: Fulano da Silva, fulanos@insper.edu.br
+# - aluno B: Sicrano de Almeida, sicranoa1@insper.edu.br
+
+def carregar_cenarios():
+    cenarios = {
+        "inicio": {
+            "titulo": "Saguao do perigo",
+            "descricao": "Voce esta no saguao de entrada do insper",
+            "opcoes": {
+                "andar professor": "Tomar o elevador para o andar do professor",
+                "biblioteca": "Ir para a biblioteca"
+            }
+        },
+        "andar professor": {
+            "titulo": "Andar do desespero",
+            "descricao": "Voce chegou ao andar da sala do seu professor",
+            "opcoes": {
+                "inicio": "Tomar o elevador para o saguao de entrada",
+                "professor": "Falar com o professor"
+            }
+        },
+        "professor": {
+            "titulo": "O monstro do Python",
+            "descricao": "Voce foi pedir para o professor adiar o EP. "
+                         "O professor revelou que é um monstro disfarçado "
+                         "e devorou sua alma.",
+            "opcoes": {}
+        },
+        "biblioteca": {
+            "titulo": "Caverna da tranquilidade",
+            "descricao": "Voce esta na biblioteca",
+            "opcoes": {
+                "inicio": "Voltar para o saguao de entrada"
+            }
+        }
+    }
+    nome_cenario_atual = "inicio"
+    return cenarios, nome_cenario_atual
+
+
+def main():
+    print("Na hora do sufoco!")
+    print("------------------")
+    print()
+    print("Parecia uma boa idéia: vou só jogar um pouquinho/assistir Netflix/"
+        "embaçar em geral. Amanhã eu começo o EP. Mas isso não deu certo...")
+    print()
+    print("É o dia de entregar o EP e você está muuuuito atrasado! Você está "
+        "na entrada do Insper, e quer procurar o professor para pedir um "
+        "adiamento do EP (boa sorte...)")
+    print()
+
+    cenarios, nome_cenario_atual = carregar_cenarios()
+
+    game_over = False
+    while not game_over:
+        cenario_atual = cenarios[nome_cenario_atual]
+
+        # Aluno A: substitua este comentário pelo código para imprimir 
+        # o cenário atual.
+
+        opcoes = cenario_atual['opcoes']
+        if len(opcoes) == 0:
+            print("Acabaram-se suas opções! Mwo mwo mwooooo...")
+            game_over = True
+        else:
+
+            # Aluno B: substitua este comentário e a linha abaixo pelo código
+            # para pedir a escolha do usuário.
+            escolha = ""
+
+            if escolha in opcoes:
+                nome_cenario_atual = escolha
+            else:
+                print("Sua indecisão foi sua ruína!")
+                game_over = True
+
+    print("Você morreu!")
+
+
+# Programa principal.
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,7 @@
+Exercício Programa - 2019/1 - Design de Software
+------------------------------------------------
+
+Alunos: 
+- Fulano da Silva, fulanos@insper.edu.br
+- Sicrano de Almeida, sicranoa1@insper.edu.br
+
